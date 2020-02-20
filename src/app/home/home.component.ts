d7c02aba11c38a9b15d515acee438c373bfc8e02
cuongnh <nguyen.hung.cuong@astecvn.com>
2020-02-20 15:58:40 +0700

    init

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import * as utils from "tns-core-modules/utils/utils";
+import * as app from "tns-core-modules/application";
+
+declare var com: any;
 
 @Component({
     selector: "Home",
     templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
+    public asvAlexa: any;
 
     constructor() {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
-        // Init your component properties here.
+        const context = utils.ad.getApplicationContext();
+        const activity = app.android.startActivity;
+        this.asvAlexa = new com.amazon.sampleapp.AsvAlexaPlugin();
+        this.asvAlexa.init(context, activity);
+        console.log(65656565, this.asvAlexa);
+    }
+
+    public tapToTalk() {
+        console.log('tapToTalk');
+        const res = this.asvAlexa.tapToTalk();
+        console.log(res);
     }
 }
